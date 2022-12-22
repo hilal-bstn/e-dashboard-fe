e38b6c21868de136c09fb951650e2f0b22d65010
Hilal Baştan <hilalbastan@gmail.com>
2022-12-22 14:41:40 +0300

    Login/ Component created and API integration done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import SignUp from './components/SignUp';
 import PrivateComponent from './components/PrivateComponent';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -21,6 +22,8 @@ function App() {
         </Route>
         
         <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/login' element={<Login/>}/>
+
        </Routes>
       </BrowserRouter>
       <Footer/>
